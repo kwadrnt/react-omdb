334d7e62c11fa61772420a235c066cd33f09fbb9
kwadrnt <kevinctse@gmail.com>
2017-05-03 15:57:25 -0700

    query value gets updated

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,6 @@ import './index.css'
 import Search from './Search'
 
 class Home extends Component {
-
   render() {
     return (
       <div className="Home">
--- src/Search.js
+++ src/Search.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react'
 
 class Search extends Component {
+  constructor() {
+    super()
+    this.state = {
+      query: ''
+    }
+  }
+
+  onInputChange (event) {
+    console.log('searching a movie')
+    this.setState({
+      query: event.target.value
+    })
+  }
+
+  onFormSubmit (event) {
+    event.preventDefault();
+    console.log('form works!')
+  }
+
   render(){
    return (
      <div>
-       <form>
+       <form onSubmit={event => this.onFormSubmit(event)}>
          <input
+           onChange={event => this.onInputChange(event)}
            placeholder='Search Movies ...'
            type='text'
-           value='' />
+           value={this.state.query} />
          <button type='submit'>Submit!</button>
        </form>
      </div>
